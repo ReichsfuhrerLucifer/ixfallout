31e9ac5e74edbbe8f637ff7d053a880d286dc7b4
Han Myeong-Ho <thks1018@gmail.com>
2019-08-09 15:15:19 +0900

    Update ix_paper.lua

--- plugins/paper/entities/entities/ix_paper.lua
+++ plugins/paper/entities/entities/ix_paper.lua
@@ -1,19 +1,20 @@
 ENT.Type = "anim"
 ENT.PrintName = "Paper"
-ENT.Author = "Black Tea"
+ENT.Author = "Black Tea, Frosty"
 ENT.Spawnable = true
 ENT.AdminOnly = true
 ENT.Category = "Helix"
 
 if (SERVER) then
-
 	function ENT:Initialize()
 		self:SetModel("models/props_c17/paper01.mdl")
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetMoveType(MOVETYPE_VPHYSICS)
 		self:SetNetVar("text", "")
 		self:SetUseType(SIMPLE_USE)
+
 		local physicsObject = self:GetPhysicsObject()
+
 		if (IsValid(physicsObject)) then
 			physicsObject:Wake()
 		end
@@ -22,11 +23,8 @@ if (SERVER) then
 	function ENT:Use(activator)
 		activator:OpenNote( self:GetNetVar( "text" ), self, self:GetNetVar( "private" ) )
 	end
-	
 else
-
 	function ENT:Draw()
 		self:DrawModel()
 	end
-	
 end
