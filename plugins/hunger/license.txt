3825628811bf45ceb0d1d2a67e8b8318e7d9d09e
Han Myeong-Ho <thks1018@gmail.com>
2019-08-09 15:22:06 +0900

    Update license

--- plugins/hunger/license.txt
+++
@@ -1,1 +0,0 @@
-Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)
--- plugins/hunger/sh_plugin.lua
+++ plugins/hunger/sh_plugin.lua
@@ -1,3 +1,9 @@
-PLUGIN.name = "Hunger+++"
+--[[
+This work is licensed under a Creative Commons
+Attribution-ShareAlike 4.0 International License.
+Created by LiGyH.
+--]]
+
+LUGIN.name = "Hunger+++"
 PLUGIN.author = "LiGyH, Frosty"
 PLUGIN.desc = "Fabricks + hunger."
