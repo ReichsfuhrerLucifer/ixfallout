ea2f687f4c06fdb51e415f746d284ab5d46ea0c4
Han Myeong-ho <thks1018@gmail.com>
2020-06-21 00:23:37 +0900

    Fix weapons workbench

--- plugins/ixcraft/stations/sh_weaponsworkbench.lua
+++ plugins/ixcraft/stations/sh_weaponsworkbench.lua
@@ -1,4 +1,4 @@
 
 STATION.name = "Weapons workbench"
 STATION.description = "A workbench used for scrapping and building weapon mods using parts from a variety of weapons."
-STATION.model = "models/mosi/fallout4/furniture/workstations/weaponsworkbench.mdl"
+STATION.model = "models/mosi/fallout4/furniture/workstations/weaponworkbench01.mdl"
