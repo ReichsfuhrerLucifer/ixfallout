efd07181f3805dd20e0855e84eb40752a1680bfd
Han Myeong-Ho <thks1018@gmail.com>
2020-06-22 14:54:25 +0900

    Create new skillbook item base

---
+++ schema/items/base/sh_skillbooks.lua
@@ -0,0 +1,33 @@
+
+ITEM.name = "Skill Book"
+ITEM.description = "A skill book."
+ITEM.category = "Books"
+ITEM.model = "models/weapons/w_pistol.mdl"
+ITEM.width = 1
+ITEM.height = 1
+ITEM.skill = "strength"
+ITEM.amount = 1
+
+ITEM.functions.Read = {
+	icon = "icon16/book_open.png",
+	OnRun = function(item)
+		local client = item.player
+		local character = client:GetCharacter()
+		local skill = skill
+		local att = character:GetAttribute(skill, 0)
+		
+		if item.skill == "strength" then
+			local skill = "str"
+		elseif item.skill == "endurance" then
+			local skill = "end"
+		elseif item.skill == "intelligence" then
+			local skill == "int"
+		elseif item.skill == "agility" then
+			local skill == "stm"
+		elseif item.skill == "luck" then
+			local skill == "lck"
+		end
+
+		client:SetAttrib(skill, math.max(att + amount, 0))
+	end
+}
